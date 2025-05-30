d4a2b72bf67eaa3e3aa0a4dc126e9fa9ac10cd23
qvimexe <qvimexe@gmail.com>
2025-05-30 23:15:06 +0300

    create scss module

--- src/components/Card/index.jsx
+++ src/components/Card/index.jsx
@@ -1,9 +1,9 @@
-import React from "react";
+import styles from "./Card.module.scss";
 
 export default function Card(props) {
   return (
-    <div className="card mb-20">
-      <div className="favorite">
+    <div className={styles.card}>
+      <div className={styles.favorite}>
         <img src="/img/heart-unliked.svg" alt="unliked" />
       </div>
       <img width={133} height={112} src={props.image} alt="Sneakers" />
