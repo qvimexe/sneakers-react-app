2f6ae2776d6ec0671e5157794b89c9f3e9fc6168
qvimexe <qvimexe@gmail.com>
2025-05-30 22:16:21 +0300

    fix components bugs

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,59 @@ import Card from "./components/Card";
 import Header from "./components/Header";
 import Drawer from "./components/Drawer";
 
+const arr = [
+  {
+    name: "Мужские Кроссовки Nike Blazer Mid Suede",
+    price: 12999,
+    imageUrl: "/img/sneakers/1.jpg",
+  },
+  {
+    name: "Мужские Кроссовки Nike Air Max 270",
+    price: 12999,
+    imageUrl: "/img/sneakers/2.jpg",
+  },
+  {
+    name: "Мужские Кроссовки Nike Blazer Mid Suede",
+    price: 8499,
+    imageUrl: "/img/sneakers/3.jpg",
+  },
+  {
+    name: "Кроссовки Puma X Aka Boku Future Rider",
+    price: 8999,
+    imageUrl: "/img/sneakers/4.jpg",
+  },
+  {
+    name: "Мужские Кроссовки Under Armour Curry 8",
+    price: 15199,
+    imageUrl: "/img/sneakers/5.jpg",
+  },
+  {
+    name: "Мужские Кроссовки Nike Kyrie 7",
+    price: 11299,
+    imageUrl: "/img/sneakers/6.jpg",
+  },
+  {
+    name: "Мужские Кроссовки Jordan Air Jordan 11",
+    price: 10799,
+    imageUrl: "/img/sneakers/7.jpg",
+  },
+  {
+    name: "Мужские Кроссовки Nike LeBron XVIII",
+    price: 16499,
+    imageUrl: "/img/sneakers/8.jpg",
+  },
+  {
+    name: "Мужские Кроссовки Nike Lebron XVIII Low",
+    price: 13999,
+    imageUrl: "/img/sneakers/9.jpg",
+  },
+  {
+    name: "Мужские Кроссовки Nike Kyrie Flytrap IV",
+    price: 11299,
+    imageUrl: "/img/sneakers/10.jpg",
+  },
+];
+
 function App() {
   return (
     <>
@@ -18,8 +71,14 @@ function App() {
               <input type="text" placeholder="Поиск..." />
             </div>
           </div>
-          <Card />
-          <div className="d-flex"></div>
+
+          <div className="d-flex flex-wrap ">
+            {arr.map((obj) => {
+              return (
+                <Card title={obj.name} price={obj.price} image={obj.imageUrl} />
+              );
+            })}
+          </div>
         </section>
       </div>
     </>
--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,17 +1,17 @@
 import React from "react";
 
-export default function Card() {
+export default function Card(props) {
   return (
-    <div className="card">
+    <div className="card mb-20">
       <div className="favorite">
         <img src="/img/heart-unliked.svg" alt="unliked" />
       </div>
-      <img width={133} height={112} src="/img/sneakers/1.jpg" alt="Sneakers" />
-      <h5>Мужские Кроссовки Nike Blazer Mid Suede</h5>
+      <img width={133} height={112} src={props.image} alt="Sneakers" />
+      <h5>{props.title}</h5>
       <div className="d-flex justify-between align-center">
         <div className="d-flex flex-column">
           <span>Цена:</span>
-          <b>12 999 грн.</b>
+          <b>{props.price}</b>
         </div>
         <button className="button">
           <img src="/img/plus.svg" width={11} height={11} alt="" />
